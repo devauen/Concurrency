3925668b01404c14f4d6e76422114e94c2d5b974
Marcin Filipowski <proartz.devon@gmail.com>
2019-02-19 10:24:18 +0100

    Add a Runnable object with a Run method

---
+++ src/com/proartz/Concurrency.java
@@ -0,0 +1,12 @@
+package com.proartz;
+
+public class Concurrency implements Runnable{
+
+    public void run() {
+        System.out.println("Hello from a thread!");
+    }
+
+    public static void main(String[] args) {
+        (new Thread(new Concurrency())).start();
+    }
+}
